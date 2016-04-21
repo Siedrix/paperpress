095ea0df5d6bd8d3e7a21f50f4cd2c3b6bbc9784
Rogelio Alberto <i@rog.mx>
2016-04-20 20:19:41 -0500

    Fix Feed generator on Paperpress - Update Feed dependency - Put back the dependency on Paperpress - Add feed to koa example

--- examples/koa/server.js
+++ examples/koa/server.js
@@ -1,6 +1,6 @@
 const koa = require('koa')
 const route = require('koa-route')
-const Paperpress = require('paperpress').Paperpress
+const Paperpress = require('../../paperpress').Paperpress
 const _ = {
   findWhere: require('lodash.findwhere')
 }
@@ -11,18 +11,21 @@ const paperpress = new Paperpress({
   uriPrefix: '/blog'
 })
 
+const feedDescription = require('./feed-description.json')
+
 paperpress.load()
 app.use(route.get('/', list))
+app.use(route.get('/feed', feed))
 app.use(route.get('/post/:slug', show))
 
-// Route Definitions //
+/** Route Definitions **/
 
-/** Post listing */
+// Post listing //
 function * list (ctx) {
   this.body = yield paperpress.getCollection('articles')
 }
 
-/** Show post :slug */
+// Show post :slug //
 function * show (slug) {
   const articles = paperpress.getCollection('articles')
   const article = _.findWhere(articles, {type: 'articles', path: slug})
@@ -32,6 +35,16 @@ function * show (slug) {
   this.body = yield article
 }
 
-// Listen //
+// Blog Feed //
+function * feed (ctx) {
+  const articles = paperpress.getCollection('articles')
+  articles.forEach((item) => {
+    item.suggestedUri = '/blog/' + item.slug
+  })
+  this.type = 'text/xml; charset=utf-8'
+  this.body = Paperpress.helpers.createFeed(feedDescription, articles).render('rss-2.0')
+}
+
+/** Listen **/
 app.listen(3000)
 console.log('[koa-paperpress] Listening on port 3000')
